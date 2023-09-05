b5d2445a4c5721f805e06cb856a0593e34b24fb6
gaurav2231 <gaurav1713108@akgec.ac.in>
2023-09-05 14:46:12 +0530

    first controller

---
+++ src/main/java/com/example/demo/test/Data.java
@@ -0,0 +1,18 @@
+package com.example.demo.test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1")
+public class Data {
+
+    @GetMapping(path = "/get-data")
+    public JsonNode returnData(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.createObjectNode().put("Name", "Ramit").put("Age", 24);
+    }
+}
